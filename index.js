537f45cfba352591d05535d01bcb7a001f9a65db
Janardhan Majumder <subrotomojumder03@gmail.com>
2022-11-24 20:14:51 +0600

    save user and return jwt token

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-async function run(){
-    app.get('/', (req, res)=> {
-        res.send('resell product server is running')
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uxk5wr6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    const usersCollection = client.db("mobile-resell").collection("users")
+    // handle user
+    app.put('/users/:email', async (req, res) => {
+        const email = req.params.email;
+        const filter = { email: email };
+        const options = { upsert: true };
+        const user = req.body;
+        const updateDoc = {
+            $set: user,
+        };
+        const results = await usersCollection.updateOne(filter, updateDoc, options);
+        const token = jwt.sign({email }, process.env.SECRET_TOKEN, {expiresIn: '1d'});
+        res.send({results, token})
     })
     
-}
-run().catch(e=> console.log(e))
+    app.get('/', (req, res) => {
+        res.send('resell product server is running')
+    })
 
-app.listen(port, ()=> {
+}
+run().catch(e => console.log(e))
+
+app.listen(port, () => {
     console.log('resell server port:', port)
 })
